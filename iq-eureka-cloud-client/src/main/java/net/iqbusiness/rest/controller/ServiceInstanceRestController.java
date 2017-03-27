cd8a67e47f83cf6f61171c885c4469cbdd39b8e8
Lesiba <lesiba.ramphele@gmail.com>
2017-03-27 18:15:01 +0200

    Add files via upload
    Committing client source code

---
+++ iq-eureka-cloud-client/src/main/java/net/iqbusiness/rest/controller/ServiceInstanceRestController.java
@@ -0,0 +1,26 @@
+package net.iqbusiness.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by lesiba on 3/27/17.
+ */
+@RestController
+public class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable final String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
